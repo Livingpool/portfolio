7851265f4e81b90d3ca3e8d943b4a14fa7e2fbfc
Livingpool <52132459+Livingpool@users.noreply.github.com>
2024-02-10 10:34:45 +0800

    fix About page animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
 import { Home, About, Projects, Contact, Navbar } from './components';
 import { Element } from 'react-scroll';
-import { useAnimation } from 'framer-motion';
 
 export default function App() {
-  const controlsHome = useAnimation();
-  const controlsAbout = useAnimation();
-  const controlsProjects = useAnimation();
-  const controlsContact = useAnimation();
-
   return (
     <main>
       <Navbar />
-      <Element name="home" onSetActive={() => controlsHome.start('show')}>
-        <Home controls={controlsHome} />
+      <Element name="home">
+        <Home />
       </Element>
-      <Element name="about" onSetActive={() => controlsAbout.start('show')}>
-        <About controls={controlsAbout} />
+      <Element name="about">
+        <About />
       </Element>
-      <Element name="projects" onSetActive={() => controlsProjects.start('show')}>
-        <Projects controls={controlsProjects} />
+      <Element name="projects">
+        <Projects />
       </Element>
-      <Element name="contact" onSetActive={() => controlsContact.start('show')}>
-        <Contact controls={controlsContact} />
+      <Element name="contact">
+        <Contact />
       </Element>
     </main>
   );
